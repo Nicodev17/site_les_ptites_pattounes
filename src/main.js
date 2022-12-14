be1ed0e37bc0ba977fcf19df4336bcd935666a6f
Nicolas Sartelet <nicolas.sartelet.mmi@gmail.com>
2022-12-14 22:54:32 +0100

    Mise en ligne site version 1.1.1

---
+++ src/main.js
@@ -0,0 +1,70 @@
+// Barre de navigation
+
+const link = document.querySelectorAll('.part-link');
+const content = document.querySelectorAll('.content');
+const contentsPart = document.querySelector('.contents-part');
+
+link.forEach((element, index) => {
+    element.addEventListener('click', event => {
+
+        link.forEach(ele => {
+            ele.classList.remove('active');
+        });
+        link[index].classList.add('active');
+
+        content.forEach(el => {
+            el.classList.remove('active');
+            el.style.display = 'none';
+        });
+        content[index].classList.add('active');
+        content[index].style.display = 'flex';
+    });
+});
+
+// Gallerie photo
+
+addingImagesBeforeAfter();
+function addingImagesBeforeAfter() {
+
+    for (let i = 1; i <= 8; i++) {
+        const newArticle = document.createElement("article");
+        newArticle.classList.add("photo-listing");
+        // const newTitle = document.createElement("a");
+        // newTitle.innerHTML = "Berger";
+        // newTitle.classList.add("photo-title");
+        // newTitle.setAttribute("href", "#");
+        // newArticle.appendChild(newTitle);
+        const newDivImg = document.createElement("div");
+        newArticle.appendChild(newDivImg);
+        newDivImg.classList.add("photo-image");
+        const newImg = document.createElement("img");
+        newDivImg.appendChild(newImg);
+
+        newImg.setAttribute("src", "media/gallery-before-after/chien-" + i + ".jpg");
+        document.querySelector(".grid-container").appendChild(newArticle);
+
+        console.log(newImg.src);
+   }
+
+}
+
+addingImagesOther();
+function addingImagesOther() {
+
+    for (let i = 1; i <= 4; i++) {
+        const newArticle = document.createElement("article");
+        newArticle.classList.add("photo-listing");
+        const newDivImg = document.createElement("div");
+        newArticle.appendChild(newDivImg);
+        newDivImg.classList.add("photo-image");
+        const newImg = document.createElement("img");
+        newDivImg.appendChild(newImg);
+
+        newImg.setAttribute("src", "media/gallery-other/chien-" + i + ".jpg");
+        document.querySelector(".grid-container-bis").appendChild(newArticle);
+
+        console.log(newImg.src);
+   }
+
+}
+
